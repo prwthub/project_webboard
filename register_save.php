855c7ddd811f8b5f1227296e4ff70dd862d03139
Spring1st <atomsuedao@gmail.com>
2022-10-26 12:04:10 +0700

    connect database fix password sha1

--- register_save.php
+++ register_save.php
@@ -8,6 +8,8 @@
     $email = $_POST["email"];
 
     $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
+    // เข้ารหัส password
+    $password = sha1($password);
 
     $sql = "SELECT * FROM user where login = '$login'";
     $result = $conn->query($sql);
@@ -17,9 +19,10 @@
     }
     // ถ้าไม่มี (ไม่ซ้ำ)
     else{
-         $sql = "INSERT INTO user(login,password,name,gender,email,role) 
+         // check อีกที ไม่แน่ใจ
+         $sql1 = "INSERT INTO user(login,password,name,gender,email,role) 
          VALUES ('$login','$password','$name','$gender','$email','m')";
-         $conn->exec($sql);
+         $conn->exec($sql1);
          $_SESSION["add_login"] = "success";
     }
     $conn = null;
