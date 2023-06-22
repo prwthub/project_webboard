62840c584ad47846e742eb225a7857d06b10f81f
prwt <perawit37@gmail.com>
2023-06-22 12:04:26 +0200

    Revert "fianl webboard"
    This reverts commit 75a95dc0f4a2a09904c21c24cb8e9f0d404e6ba7.

--- _test.php
+++ _test.php
@@ -12,36 +12,4 @@
         echo "<BR><BR>";
     }
 
-
-
-    $conn = new PDO("mysql:host=localhost; dbname=webboard; charset=utf8" , "root" , "");
-    $query = $conn->query("SELECT * FROM user");
-    while($result = $query->fetch(PDO::FETCH_ASSOC)){
-        echo $result['username'];
-        echo $result['password'];
-    }
-
-    
-    $conn = new PDO("mysql:host=localhost; dbname=webboard; charset=utf8" , "root" , "");
-    $query = $conn->query("SELECT * FROM user where username = 'perawit'");
-    $result = $query->fetch(PDO::FETCH_ASSOC);
-    
-    echo "username : ".$result["username"];
-    echo "password : ".$result["password"];
-    
-
-
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-    
-    $conn = new PDO("mysql:host=localhost; dbname=webboard; charset=utf8" , "root" , "");
-    
-    $sql = "INSERT INTO user(username,password) VALUES ('$username','$password')";
-    
-    $conn->exec($sql);
-    
-
-
-
-
 ?>
